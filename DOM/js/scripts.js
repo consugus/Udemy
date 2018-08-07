807c1178a70f88b91cd46458003741a2dc3dcf6c
Gustavo <consugus@gmail.com>
2018-08-07 20:06:59 -0300

    Proyecto Sitio de Conferencias - HTML, CSS y DOM listo para comenzar a programar con Javascript

--- DOM/js/scripts.js
+++ DOM/js/scripts.js
@@ -1,4 +1,14 @@
+console.log("1");
+
 (function(){
   'use strict';
-  document.addEventListener('DOMContentLoaded', function(){  });
+  document.addEventListener('DOMContentLoaded', function(){
+    // console.log("2");
+    console.log(document.querySelector("aside h2").innerHTML ="Hola");
+
+  });
+
 })();
+
+console.log("3");
+
---
+++ Proyecto Sitio de Conferencias/js/main.js
@@ -0,0 +1,9 @@
+(function(){
+    "use strict"
+    document.addEventListener("DOMContentLoaded", function(){
+
+        //Acá va el código a ejecutarse después que se carge el documento
+        // console.log("DOM listo");
+
+    });
+})();
