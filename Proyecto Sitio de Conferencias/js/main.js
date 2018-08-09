d946b8b6c2ff17ead9994efbd3641ab5049d1f5b
Gustavo <consugus@gmail.com>
2018-08-09 00:56:58 -0300

    Proyecto Sitio de Conferencias - Mapa con la Ubicacíon Completado

--- Proyecto Sitio de Conferencias/js/main.js
+++ Proyecto Sitio de Conferencias/js/main.js
@@ -5,6 +5,16 @@
 
     document.addEventListener("DOMContentLoaded", function(){
 
+        var map = L.map('mapa').setView([-31.423129, -64.152144], 15);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([-31.423129, -64.152144]).addTo(map)
+            .bindPopup('La casa del Consugus')
+            .openPopup();
+
         // Variables de Datos de Usuario
         var nombre = document.getElementById("nombre");
         var apellido = document.getElementById("apellido");
